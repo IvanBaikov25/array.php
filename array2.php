1c67f3e05e40f42e477c1fd7de767d64bf9e6080
IvanBaikov25 <bajkovivan07@gmail.com>
2025-03-02 17:32:15 +0300

    Update array2.php

--- array2.php
+++ array2.php
@@ -1,17 +1,16 @@
 <?php
-$numbers = [9, 35, 4, 20, 34, 82, 54];
-function findThirdMax($numbers) {
-    $uniqueNumbers = array_unique($numbers);
-    rsort($uniqueNumbers);
-    if (count($uniqueNumbers) < 3) {
-        return null; 
+
+$students = [ 
+    ['name' => 'Маша', 'age' => 22, 'grades' => [5, 4, 5]],
+    ['name' => 'Витя', 'age' => 23, 'grades' => [3, 4, 2]],
+    ['name' => 'Олег', 'age' => 21, 'grades' => [4, 5, 5]], 
+];
+
+foreach ($students as $student) {
+    $average = array_sum($student['grades']) / count($student['grades']);
+    
+    if ($average >= 4) {
+        echo $student['name'] . ': ' . number_format($average, 2) . ' ';
     }
-    return $uniqueNumbers[2];
 }
-$thirdMax = findThirdMax($numbers);
-if ($thirdMax !== null) {
-    echo "Третье максимальное число: " . $thirdMax;
-} else {
-    echo "В массиве недостаточно уникальных чисел для нахождения третьего максимума.";
-}
-?>
+
